1afd72b09d39f9856a8595f454da0d960d5ddab6
Viktor Matrai <viktor0matrai@gmail.com>
2019-04-25 20:06:18 +0200

    tests for sumbission type and expected outcome

--- test/index.js
+++ test/index.js
@@ -7,4 +7,20 @@ test('Turnaround type is not an integer', testIt =>{
    testIt.throws(() => dueDateCalculator(day, true), 'Turnaround type should be an integer');
    testIt.throws(() => dueDateCalculator(day, {Key: 'value'}), 'Turnaround type should be an integer');
    testIt.throws(() => dueDateCalculator(day, 0.1), 'Turnaround type should be an integer');
-})
+});
+
+test('Wrong type of submission date', testIt=> {
+   testIt.throws(() => dueDateCalculator('wrong type', 1), 'Submission date should be a Date');
+   testIt.throws(() => dueDateCalculator(1.1, 1), 'Submission date should be a Date');
+   testIt.throws(() => dueDateCalculator(true, 1), 'Submission date should be a Date');
+});
+
+test('Ending on the same day', testIt=>{
+   const dateOfSumbmission = new Date(2019, 4, 25, 19,38,0,0);
+   const turnaround = 1;
+
+   const dueDate = dueDateCalculator(dateOfSumbmission, turnaround);
+   const expected = new Date(2019, 4, 25, 20,38,0,0);
+
+   testIt.is(dueDate, expected);
+});
