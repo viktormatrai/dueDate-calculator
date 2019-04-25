be274b6d2550237d09f3e0fa9b0f74c77aaec139
Viktor Matrai <viktor0matrai@gmail.com>
2019-04-25 17:40:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const STARTING_HOUR = 9;
+const END_HOUR= 17;
+const ONE_HOUR_IN_MS = 60*60*1000;
+
+function isWeekend(startingDate) {
+    const currentDay = startingDate.getDay();
+    return(currentDay === 6 || currentDay ===0);
+}
+
+module.exports = (dateOfSubmission, turnaround);{
+    const remainingTime = 0;
+    return justInWorkingHours(dateOfSubmission, remainingTime)
+}
+
+function justInWorkingHours(startingDate, remainingTime) {
+
+}
