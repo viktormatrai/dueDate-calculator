eb0125a25e85022ff904f6801f21414cc41fa6a8
Viktor Matrai <viktor0matrai@gmail.com>
2019-04-26 10:47:12 +0200

    submission on friday with 16h turnaround test

--- test/index.js
+++ test/index.js
@@ -11,17 +11,27 @@ test('Turnaround type is not an integer', testIt =>{
 });
 
 test('Wrong type of submission date', testIt=> {
-   testIt.throws(() => index.dueDateCalculator('wrong type', 1), 'Submission date should be a Date');
+   testIt.throws(() => index.dueDateCalculator('string', 1), 'Submission date should be a Date');
    testIt.throws(() => index.dueDateCalculator(1.1, 1), 'Submission date should be a Date');
    testIt.throws(() => index.dueDateCalculator(true, 1), 'Submission date should be a Date');
 });
 
 test('Ending on the same day', testIt=>{
-   const dateOfSumbmission = new Date(2019, 4, 25, 19,38,0,0);
-   const turnaround = 1;
+   const dateOfSubmission = new Date(2019, 3, 24, 11,38,0,0);
+   const turnaround = 2;
 
-   const dueDate = index.dueDateCalculator(dateOfSumbmission, turnaround);
-   const expected = new Date(2019, 4, 26, 19,38,0,0);
+   const dueDate = index.dueDateCalculator(dateOfSubmission, turnaround);
+   const expected = new Date(2019, 3, 26, 11,38,0,0);
 
    testIt.is(+dueDate, +expected);
+});
+
+test('Submission time on Friday, due date to Tuesday', testIs => {
+    const dateOfSubmission = new Date(2019, 3, 26, 16, 0, 0, 0);
+    const turnaround = 16;
+
+    const dueDate = index.dueDateCalculator(dateOfSubmission, turnaround);
+    const expected = new Date(2019, 3, 30, 10, 0, 0, 0);
+
+    testIs.is(+dueDate, +expected)
 });
