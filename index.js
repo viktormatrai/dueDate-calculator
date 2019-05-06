82d413241fdef83787bc9be4bda7d47c0c45e84e
Viktor Matrai <viktor0matrai@gmail.com>
2019-05-06 07:07:02 +0200

    stack error fixed

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const STARTING_HOUR = 9;
-const END_HOUR= 17;
-const ONE_HOUR_IN_MS = 60*60*1000;
+const ONE_HOUR_MS = 60 * 60 * 1000;
+const START_HOUR = 9;
+const END_HOUR = 17;
 
-exports.dueDateCalculator = (dateOfSubmission, turnaround) => {
-    if (!(dateOfSubmission instanceof Date)){
-        throw new Error('Date of submission must be an instance of Date')
-    }
-    if(turnaround !== parseInt(turnaround, 10)){
-        throw new Error('Turnaround must be an integer')
-    }
-    const remainingTime = turnaround * ONE_HOUR_IN_MS;
-    return reduceToWorkingHours(dateOfSubmission, remainingTime)
-};
+function isWeekend (startDate) {
+    const currentDay = startDate.getDay();
+    return (currentDay === 6) || (currentDay === 0)
+}
 
-const isWeekend = startingDate =>{
-    const currentDay = startingDate.getDay();
-    return(currentDay === 6) || (currentDay ===0);
-};
+function reduceHours (startDate, remainingTime) {
+    const startTime = +startDate
 
-const reduceToWorkingHours = (startingDate, remainingTime) =>{
-    const startingTime = startingDate;
-
-    /*  checks if the original starting date is on a weekend,
-        if it is, it keeps adding 24hrs to it until it's on a weekday */
-    if (isWeekend(startingDate)){
-        const incrementWithADay = 24*ONE_HOUR_IN_MS;
-        const nextDay = new Date(startingTime + incrementWithADay);
-        return reduceToWorkingHours(nextDay, remainingTime);
+    if (isWeekend(startDate)) {
+        const nextDay = new Date(startTime + 24 * ONE_HOUR_MS);
+        return reduceHours(nextDay, remainingTime)
     }
 
-    const endTime = startingDate.setHours(END_HOUR, 0,0,0);
-    const difference = endTime - startingTime;
-    if (difference >= remainingTime){
-        return new Date(startingTime + remainingTime);
+    const endTime = startDate.setHours(END_HOUR, 0, 0, 0);
+    const difference = endTime - startTime;
+
+    if (difference >= remainingTime) {
+        return new Date(startTime + remainingTime)
     }
 
-    const nextDay = new Date(endTime + (24 - (END_HOUR - STARTING_HOUR)) * ONE_HOUR_IN_MS);
-    return reduceToWorkingHours(nextDay, remainingTime - difference)
+    const nextDay = new Date(endTime +
+        (24 - (END_HOUR - START_HOUR)) * ONE_HOUR_MS);
+    return reduceHours(nextDay, remainingTime - difference)
+}
+
+exports.dueDateCalculator = (submitDate, turnaround) => {
+    if (!(submitDate instanceof Date)) {
+        throw new Error('Submit date must be instance of Date')
+    }
+
+    if (turnaround !== parseInt(turnaround, 10)) {
+        throw new Error('Turnaround must be integer')
+    }
+
+    const remainingTime = turnaround * ONE_HOUR_MS;
+
+    return reduceHours(submitDate, remainingTime)
 };
--- test/index.js
+++ test/index.js
@@ -1,37 +1,81 @@
-const test = require('ava');
-const index = require('../index');
+const test = require('ava')
+const dd = require('../index');
 
-
-test('Turnaround type is not an integer', testIt =>{
-   const day = new Date();
-   testIt.throws(() => index.dueDateCalculator(day, 'wrong type'), 'Turnaround type should be an integer');
-   testIt.throws(() => index.dueDateCalculator(day, true), 'Turnaround type should be an integer');
-   testIt.throws(() => index.dueDateCalculator(day, {Key: 'value'}), 'Turnaround type should be an integer');
-   testIt.throws(() => index.dueDateCalculator(day, 0.1), 'Turnaround type should be an integer');
+test('wrong submit date type', t => {
+    t.throws(() => dd.dueDateCalculator('foo', 0),
+        'Submit date must be instance of Date');
+    t.throws(() => dd.dueDateCalculator(123, 0),
+        'Submit date must be instance of Date');
+    t.throws(() => dd.dueDateCalculator(false, 0),
+        'Submit date must be instance of Date')
 });
 
-test('Wrong type of submission date', testIt=> {
-   testIt.throws(() => index.dueDateCalculator('string', 1), 'Submission date should be a Date');
-   testIt.throws(() => index.dueDateCalculator(1.1, 1), 'Submission date should be a Date');
-   testIt.throws(() => index.dueDateCalculator(true, 1), 'Submission date should be a Date');
+test('wrong turnaround type', t => {
+    const d = new Date()
+
+    t.throws(() => dd.dueDateCalculator(d, 'foo'), 'Turnaround must be integer');
+    t.throws(() => dd.dueDateCalculator(d, false), 'Turnaround must be integer');
+    t.throws(() => dd.dueDateCalculator(d, { a: 'b' }),
+        'Turnaround must be integer');
+    t.throws(() => dd.dueDateCalculator(d, 1.23), 'Turnaround must be integer')
 });
 
-test('Ending on the same day', testIt=>{
-   const dateOfSubmission = new Date(2019, 3, 24, 11,38,0,0);
-   const turnaround = 2;
+test('same day ending', t => {
+    const submitDate = new Date(2016, 10, 24, 12, 40, 0, 0);
+    const turnaround = 2;
 
-   const dueDate = index.dueDateCalculator(dateOfSubmission, turnaround);
-   const expected = new Date(2019, 3, 26, 11,38,0,0);
+    const due = dd.dueDateCalculator(submitDate, turnaround);
+    const exp = new Date(2016, 10, 24, 14, 40, 0, 0);
 
-   testIt.is(+dueDate, +expected);
+    t.is(+due, +exp)
 });
 
-test('Submission time on Friday, due date to Tuesday', testIs => {
-    const dateOfSubmission = new Date(2019, 3, 26, 16, 0, 0, 0);
-    const turnaround = 16;
+test('just barely not same day ending', t => {
+    const submitDate = new Date(2016, 10, 24, 16, 10, 0, 0);
+    const turnaround = 1;
 
-    const dueDate = index.dueDateCalculator(dateOfSubmission, turnaround);
-    const expected = new Date(2019, 3, 30, 10, 0, 0, 0);
+    const due = dd.dueDateCalculator(submitDate, turnaround);
+    const exp = new Date(2016, 10, 25, 9, 10, 0, 0);
 
-    testIs.is(+dueDate, +expected)
+    t.is(+due, +exp)
 });
+
+test('between two working days', t => {
+    const submitDate = new Date(2016, 10, 24, 12, 40, 0, 0);
+    const turnaround = 9;
+
+    const due = dd.dueDateCalculator(submitDate, turnaround);
+    const exp = new Date(2016, 10, 25, 13, 40, 0, 0);
+
+    t.is(+due, +exp)
+});
+
+test('friday to monday', t => {
+    const submitDate = new Date(2016, 10, 25, 12, 40, 0, 0);
+    const turnaround = 10;
+
+    const due = dd.dueDateCalculator(submitDate, turnaround)
+    const exp = new Date(2016, 10, 28, 14, 40, 0, 0);
+
+    t.is(+due, +exp)
+})
+
+test('multiple weeks', t => {
+    const submitDate = new Date(2016, 10, 24, 12, 40, 0, 0);
+    const turnaround = 16 * 8 + 3;
+
+    const due = dd.dueDateCalculator(submitDate, turnaround)
+    const exp = new Date(2016, 11, 16, 15, 40, 0, 0);
+
+    t.is(+due, +exp)
+});
+
+test('end of day ending', t => {
+    const submitDate = new Date(2016, 11, 16, 12, 0, 0, 0)
+    const turnaround = 5
+
+    const due = dd.dueDateCalculator(submitDate, turnaround)
+    const exp = new Date(2016, 11, 16, 17, 0, 0, 0)
+
+    t.is(+due, +exp)
+})
